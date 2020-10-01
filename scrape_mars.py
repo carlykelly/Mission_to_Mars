ece8d70976930415a7e149a513da783c19b2aedb
Carly Kelly <carlyfkelly@gmail.com>
2020-10-01 11:42:18 -0400

    adding comments

--- scrape_mars.py
+++ scrape_mars.py
@@ -20,6 +20,7 @@ def scrape():
     
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(url)
+    #sleep is used to ensure page has time to load before scraping attempt.
     time.sleep(2)
     
     
