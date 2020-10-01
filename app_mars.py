0522399f77be38e80ce6cc09e85a2d9630dc2bbd
Carly Kelly <carlyfkelly@gmail.com>
2020-10-01 11:49:18 -0400

    editing spacing

--- app_mars.py
+++ app_mars.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_mission"
 mongo = PyMongo(app)
 
-
-
 @app.route("/")
 def index():
     mars_info = mongo.db.mars_info.find_one()
